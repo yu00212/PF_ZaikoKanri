4f9f9768e41baa5903ae2f66eb7ed0bc07269f36
yusei <yu2000212@icloud.com>
2021-01-08 20:26:44 +0900

    インデント統一

--- sanitize/sanitize.php
+++ sanitize/sanitize.php
@@ -1,19 +1,22 @@
 <?php
 
-function sanitize($before) {
-
-    if (is_array($before)) {
-        
+function sanitize($before) 
+{
+    if (is_array($before)) 
+    {
         $after = array();  
 
-        foreach ($before as $key=>$value) {
-            $after[$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+        foreach ($before as $key=>$value) 
+        {
+          $after[$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
         }
 
         return $after;
 
-    }else {
-        return htmlspecialchars($before, ENT_QUOTES, 'UTF-8');
+    }
+    else 
+    {
+      return htmlspecialchars($before, ENT_QUOTES, 'UTF-8');
     }
 
 } 
