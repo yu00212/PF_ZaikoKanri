6cdaf5c43310070e90680efa7dd0fdb3dd6e23a4
yusei <yu2000212@icloud.com>
2021-01-09 13:57:04 +0900

    インデント統一

--- sanitize/sanitize.php
+++ sanitize/sanitize.php
@@ -1,10 +1,10 @@
 <?php
-
+//削除してエスケープ処理変える
 function sanitize($before) {
 
     if (is_array($before)) {
-        
-        $after = array();  
+
+        $after = array();
 
         foreach ($before as $key=>$value) {
             $after[$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
@@ -16,7 +16,13 @@ function sanitize($before) {
         return htmlspecialchars($before, ENT_QUOTES, 'UTF-8');
     }
 
-} 
+}
+
+//htmlspecialchars関数
+function h($s)
+{
+  return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
+}
 
 ?>
 
