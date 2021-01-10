0e86cc7c68e5f7afc8af23848a517616e6dde415
yusei <yu2000212@icloud.com>
2021-01-10 21:17:28 +0900

    [update]コード整理
    コードが見づらかったためイコール部分にインデントを入れて統一しました。

--- user_login/login_check.php
+++ user_login/login_check.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang = "ja">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="../public/css/common.css">
+<meta charset = "UTF-8">
+<meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
+<link rel = "stylesheet" href = "../public/css/common.css">
 <title>ログイン</title>
 </head>
 <body>
@@ -11,50 +11,50 @@
 <?php
 require_once('../db_connect/db_connect.php');
 require_once('../sanitize/sanitize.php');
-$post=sanitize($_POST);
+$post = sanitize($_POST);
 
 if (!empty($post['email']))
 {
-  $user_email=$post['email'];
+  $user_email = $post['email'];
 }
 
 if (!empty($post['pass']))
 {
-  $user_pass=$post['pass'];
+  $user_pass = $post['pass'];
 }
 
 if(isset($user_pass))
 {
-  $user_pass=md5($user_pass);
+  $user_pass = md5($user_pass);
 }
 
 try
 {
-  $sql='SELECT name FROM users WHERE email=? AND password=?';
-  $stmt=connect()->prepare($sql);
-  $data[]=$user_email;
+  $sql = 'SELECT name FROM users WHERE email = ? AND password = ?';
+  $stmt = connect()->prepare($sql);
+  $data[] = $user_email;
 
   if(isset($user_pass))
   {
-    $data[]=$user_pass;
+    $data[] = $user_pass;
   }
 
   $stmt->execute($data);
-  $dbh=null;
-  $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+  $dbh = null;
+  $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 
-  if($rec==false)
+  if($rec == false)
   {
     print'メールアドレスかパスワードが間違っています。<br />';
-    print'<a href="login_form.html">戻る</a>';
+    print'<a href = "login_form.html">戻る</a>';
   }
   else
   {
     session_start();
-    $_SESSION['login']=1;
-    $_SESSION['user_email']=$user_email;
-    $_SESSION['user_pass']=$user_pass;
-    $_SESSION['user_name']=$rec['name'];
+    $_SESSION['login'] = 1;
+    $_SESSION['user_email'] = $user_email;
+    $_SESSION['user_pass'] = $user_pass;
+    $_SESSION['user_name'] = $rec['name'];
     header('Location:../public/list.php');
     exit();
   }
--- user_login/user_logout_check.php
+++ user_login/user_logout_check.php
@@ -4,19 +4,19 @@ certification();
 ?>
 
 <!DOCTYPE html>
-<html lang="ja">
+<html lang = "ja">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="../public/css/common.css">
+<meta charset = "UTF-8">
+<meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
+<link rel = "stylesheet" href = "../public/css/common.css">
 <title>ログアウト</title>
 </head>
 <body>
 
 <p>ログアウトしますか？</p>
-<form action="user_logout.php">
-<input type="button" onclick="history.back()" value="戻る">
-<input type="submit" value="はい">
+<form action = "user_logout.php">
+<input type = "button" onclick = "history.back()" value = "戻る">
+<input type = "submit" value = "はい">
 </form>
 
 </body>
