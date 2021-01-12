c6ee6fd29c12dfc5406f1679da9e3c2737b161a9
yusei <yu2000212@icloud.com>
2021-01-12 21:20:56 +0900

    [update]在庫検索機能のブラウザ表示修正
    検索ワードによって適切な結果をブラウザ表示するよう条件分岐を追加しました。

--- public/stock_search.php
+++ public/stock_search.php
@@ -19,41 +19,46 @@ require_once('../db_connect/db_connect.php');
 
 try
 {
-  $stmt = connect()->prepare("SELECT * FROM stocks WHERE stock_name LIKE (:stock_name) ");
-
-  if($stmt)
+  if($_SESSION['POST'] === '')
   {
-    $search_name = $_SESSION['POST'];
-    unset($_SESSION['POST'] );
+   echo '検索ワードが入力されていません。<br>';
+  }
+  else
+  {
+    $stmt = connect()->prepare("SELECT * FROM stocks WHERE stock_name LIKE (:stock_name) ");
 
-    if(isset($search_name))
+    if($stmt)
     {
-      $like_search_name = "%".$search_name."%";
-    }
+      $search_name = $_SESSION['POST'];
 
-    //プレースホルダーへ実際の値を設定する
-    if(isset($like_search_name))
-    {
-      $stmt->bindValue(':stock_name', $like_search_name, PDO::PARAM_STR);
-    }
-
-    $errors = [];
-    if($stmt->execute())
-    {
-      //レコード件数取得
-      $row_count = $stmt->rowCount();
-
-      while($row = $stmt->fetch())
+      if(isset($search_name))
       {
-        $rows[] = $row;
+        $like_search_name = "%".$search_name."%";
       }
+
+      //プレースホルダーへ実際の値を設定する
+      if(isset($like_search_name))
+      {
+        $stmt->bindValue(':stock_name', $like_search_name, PDO::PARAM_STR);
+      }
+
+      $errors = [];
+      if($stmt->execute())
+      {
+        //レコード件数取得
+        $row_count = $stmt->rowCount();
+
+        while($row = $stmt->fetch())
+        {
+          $rows[] = $row;
+        }
+      }
+      else
+      {
+        $errors['error'] = "検索失敗しました。";
+      }
+      $dbh = null;
     }
-    else
-    {
-      $errors['error'] = "検索失敗しました。";
-    }
-    //print_r($stmt -> errorInfo()); //SQLエラー内容確認
-    $dbh = null;
   }
 }
 catch(PDOException $e)
@@ -61,38 +66,43 @@ catch(PDOException $e)
 	print('Error:'.$e->getMessage());
 	$errors['error'] = "データベース接続失敗しました。";
 }
-
 ?>
 
-<?php if(count($errors) === 0): ?>
+<?php if($_SESSION['POST'] === '') : ?>
+  <?php unset($_SESSION['POST']); ?>
+<?php else : ?>
+  <?php unset($_SESSION['POST']); ?>
 
-<p><?= "「".htmlspecialchars($search_name, ENT_QUOTES, 'UTF-8')."」で検索しました。"?></p>
-<p><?= $row_count?>件の該当商品がありました。</p>
+  <?php if(count($errors) === 0): ?>
+    <p><?= "「".htmlspecialchars($search_name, ENT_QUOTES, 'UTF-8')."」で検索しました。"?></p>
 
-<table class = "sorttbl" id = "myTable" border = '1'>
-  <tr><th>ID</th><th>購入日</th><th>商品名</th><th>値段</th><th>数量</th><th>消費期限</th></tr>
-  <?php foreach ((array)$rows as $row): ?>
-    <tr>
-      <td><?=  $row['stock_id']?></td>
-      <td><?=  htmlspecialchars($row['purchase_date'], ENT_QUOTES, 'UTF-8') ?></td>
-      <td><?=  htmlspecialchars($row['stock_name'], ENT_QUOTES, 'UTF-8') ?></td>
-      <td>¥<?=  htmlspecialchars($row['price'], ENT_QUOTES, 'UTF-8') ?></td>
-      <td><?=  htmlspecialchars($row['number'], ENT_QUOTES, 'UTF-8') ?></td>
-      <td><?=  htmlspecialchars($row['deadline'], ENT_QUOTES, 'UTF-8') ?></td>
-    </tr>
-  <?php endforeach; ?>
-</table>
+      <?php if($row_count === 0) : ?>
+        <p>該当商品はありません。</p>
+      <?php else : ?>
+        <p><?= $row_count?>件の該当商品がありました。</p>
+          <table class = "sorttbl" id = "myTable" border = '1'>
+            <tr><th>ID</th><th>購入日</th><th>商品名</th><th>値段</th><th>数量</th><th>消費期限</th></tr>
+              <?php foreach ((array)$rows as $row) : ?>
+                <tr>
+                  <td><?=  $row['stock_id']?></td>
+                  <td><?=  htmlspecialchars($row['purchase_date'], ENT_QUOTES, 'UTF-8') ?></td>
+                  <td><?=  htmlspecialchars($row['stock_name'], ENT_QUOTES, 'UTF-8') ?></td>
+                  <td>¥<?=  htmlspecialchars($row['price'], ENT_QUOTES, 'UTF-8') ?></td>
+                  <td><?=  htmlspecialchars($row['number'], ENT_QUOTES, 'UTF-8') ?></td>
+                  <td><?=  htmlspecialchars($row['deadline'], ENT_QUOTES, 'UTF-8') ?></td>
+                </tr>
+              <?php endforeach; ?>
+          </table>
+      <?php endif; ?>
 
-<?php elseif(count($errors) > 0): ?>
-<?php
-foreach($errors as $value)
-{
-	echo "<p>".$value."</p>";
-}
-?>
-<?php endif; ?>
+    <?php elseif(count($errors) > 0) : ?>
+      <?php foreach($errors as $value) : ?>
+        <p><?php print $value; ?></p>
+      <?php endforeach; ?>
+    <?php endif; ?>
 
-<br />
+<?php endif; ?><br>
+
 <form>
 <input type = "button" onclick = "history.back()" value = "戻る">
 </form>
