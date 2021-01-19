993e1226a8847fb8458fc34bf1beb3ed30129098
yusei <yu2000212@icloud.com>
2021-01-19 10:54:44 +0900

    [update] 入力確認機能追加
    未入力での登録を防ぐためを入力確認機能を追加しました。

--- user/user_add.php
+++ user/user_add.php
@@ -1,37 +1,36 @@
 <!DOCTYPE html>
 <html lang = "ja">
 <head>
-　<meta charset = "UTF-8">
-　<meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
-　<link rel = "stylesheet" href = "../public/css/common.css">
-　<title>ユーザー登録</title>
+  <meta charset = "UTF-8">
+  <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
+  <link rel = "stylesheet" href = "../public/css/common.css">
+  <title>ユーザー登録</title>
 </head>
 <body>
 
 <p>全ての項目を入力後「登録」を押してください。</p>
 <p>※登録内容はログイン時に必要になります。<br>　メモしておいてください。</p>
 
-
   <form method = "post" action = "user_add_check.php">
 
   <p>
-    <label for = "name">ユーザー名：</label> <!-- label->フォームと項目名を関連付ける-->
-    <input type = "text" name = "name" style = "width:200px">
+    <label for = "name">ユーザー名<font color = "red">※</font>：</label> <!-- label->フォームと項目名を関連付ける-->
+    <input type = "text" name = "name" style = "width:200px" required>
   </p>
 
   <p>
-    <label for = "email">メールアドレス：</label>
-    <input type = "email" name = "email"> <!-- type->部品の形式 -->
+    <label for = "email">メールアドレス<font color = "red">※</font>：</label>
+    <input type = "email" name = "email" required> <!-- type->部品の形式 -->
   </p>
 
   <p>
-    <label for = "pass">パスワード：</label>
-    <input type = "password" name = "pass">
+    <label for = "pass">パスワード<font color = "red">※</font>：</label>
+    <input type = "password" name = "pass" required>
   </p>
 
   <p>
-    <label for = "pass2">パスワード再入力：</label>
-    <input type = "password" name = "pass2">
+    <label for = "pass2">パスワード再入力<font color = "red">※</font>：</label>
+    <input type = "password" name = "pass2" required>
   </p>
 
     <input type = "button" onclick = "history.back()" value = "戻る">
