af77ce477dee1186b0cd3a8dcad1eee350d37df9
yusei <yu2000212@icloud.com>
2021-02-10 22:04:07 +0900

    [update] 機能修正
    パスワードハッシュ化に脆弱性のある関数を使用していたため、ハッシュ化とパスワード認証の関数を変更しました。

--- user/user_add_done.php
+++ user/user_add_done.php
@@ -1,29 +1,27 @@
 <?php
 
 require_once '../db_connect/db_connect.php';
-
-$user_name = $_POST['name'];
-$user_email = $_POST['email'];
-$user_pass = $_POST['pass'];
-$err[] = '';
+require_once '../common/input_check.php';
 
 try
 {
+    list($user_data, $err) = validateUser($post);
+    $user_data['pass'] = password_hash($user_data['pass'], PASSWORD_DEFAULT);
     $sql = 'INSERT INTO users (name,email,password) VALUES (?,?,?)';
     $stmt = connect()->prepare($sql);
-    $data[] = $user_name;
-    $data[] = $user_email;
-    $data[] = $user_pass;
+    $data[] = $user_data['name'];
+    $data[] = $user_data['email'];
+    $data[] = $user_data['pass'];
     $stmt->execute($data);
     $dbh = null;
 } catch (Exception $e) {
     $err['exception'] = $e->getMessage();
 }
 
-$smarty->assign('user_name', $user_name);
+$smarty->assign('user_data', $user_data);
 $smarty->assign('err', $err);
 
-if (isset($err['exception']) == false) {
+if (isset($err['name']) == false && isset($err['email']) == false && isset($err['pass']) == false && isset($err['pass2']) == false && isset($err['exception']) == false) {
     $smarty->display('../smarty/templates/user/user_add_done.tpl');
 } else {
     $smarty->display('../smarty/templates/err.tpl');
--- user_login/login_check.php
+++ user_login/login_check.php
@@ -2,44 +2,49 @@
 
 require_once '../db_connect/db_connect.php';
 
-if (!empty($_POST['email'])) {
-    $user_email = $_POST['email'];
-} else {
+$reg_str = "/\A[a-z\d]{6,50}+\z/i";
+
+if ($_POST['email'] == '') {
     $err['email'] = 'メールアドレスが入力されていません。';
-}
-
-if (!empty($_POST['pass'])) {
-    $user_pass = $_POST['pass'];
+} elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $err['email'] = 'メールアドレスを正しい形式で入力してください。';
 } else {
-    $err['pass'] = 'パスワードが入力されていません。';
+    $user_email = $_POST['email'];
 }
 
-if (isset($user_pass)) {
-    $user_pass = md5($user_pass);
+if ($_POST['pass'] == '') {
+    $err['pass'] = 'パスワードが入力されていません。';
+} elseif (!preg_match($reg_str, $_POST['pass'])) {
+    $err['pass'] = 'パスワードは半角英数字6~50文字で入力してください。';
+} else {
+    $user_pass = $_POST['pass'];
 }
 
 try
 {
-    $sql = 'SELECT name FROM users WHERE email = ? AND password = ?';
+    $sql = 'SELECT * FROM users WHERE email = ?';
     $stmt = connect()->prepare($sql);
     $data[] = $user_email;
-    $data[] = $user_pass;
-
     $stmt->execute($data);
     $dbh = null;
     $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($rec == false) {
-        $err['mis'] = 'メールアドレスかパスワードが間違っています。';
+    if (isset($rec['password'])) {
+        if (password_verify($user_pass, $rec['password'])) {
+            session_start();
+            $_SESSION['login'] = 1;
+            $_SESSION['user_email'] = $user_email;
+            $_SESSION['user_pass'] = $user_pass;
+            $_SESSION['user_name'] = $rec['name'];
+            header('Location:../public/list.php');
+            exit();
+        } else {
+            $err['mis'] = 'パスワードが違います。';
+        }
     } else {
-        session_start();
-        $_SESSION['login'] = 1;
-        $_SESSION['user_email'] = $user_email;
-        $_SESSION['user_pass'] = $user_pass;
-        $_SESSION['user_name'] = $rec['name'];
-        header('Location:../public/list.php');
-        exit();
+        $err['mis'] = 'メールアドレス又はパスワードが違います。';
     }
+
 } catch (Exception $e) {
     $err['exception'] = $e->getMessage();
 }
