f0f6a7604084df922de09ef5ff09aeed931be0de
星勇成 <yusei@hoshiisamuseinoMacBook-puro.local>
2021-10-03 23:46:45 +0900

    [clean] コード整理
    コードスタイルを修正しました。

--- common/common.php
+++ common/common.php
@@ -1,11 +1,12 @@
 <?php
 
-function err_common($e, $smarty) {
-    $err['exception'] = $e->getMessage();
-    $smarty->assign('err', $err);
+function err_common($e, $smarty)
+{
+	$err['exception'] = $e->getMessage();
+	$smarty->assign('err', $err);
 
-    if (isset($err['exception'])) {
-        $smarty->display('../smarty/templates/err.tpl');
-    }
-    return;
+	if (isset($err['exception'])) {
+		$smarty->display('../smarty/templates/err.tpl');
+	}
+	return;
 }
